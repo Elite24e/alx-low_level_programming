c84934e4ba215419a7efb28561d1bad40a6178a5
Oikeh Elijah <elijahoikeh@gmail.com>
2023-06-15 15:14:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with printf
+ * Return: (0) Csuccessful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
