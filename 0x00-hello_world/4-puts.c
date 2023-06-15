ba04b16f16e21f8fea156ae61de1019f2bb08603
Oikeh Elijah <elijahoikeh@gmail.com>
2023-06-15 15:04:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main a program that print a line with puts function
+ * main - a program that print a line with puts function
  * Return: 0 (successful)
  */
 int main(void)
